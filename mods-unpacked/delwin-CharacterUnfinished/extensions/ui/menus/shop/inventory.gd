457feecd789b866d61daea407aa7d9ab1bef5bb8
Delwin444 <justinkuhn@t-online.de>
2025-10-27 23:45:35 +0100

    Update function signature after todays 1.1.13.0 'New Dawn' Update

--- mods-unpacked/delwin-CharacterUnfinished/extensions/ui/menus/shop/inventory.gd
+++ mods-unpacked/delwin-CharacterUnfinished/extensions/ui/menus/shop/inventory.gd
@@ -1,7 +1,7 @@
 extends "res://ui/menus/shop/inventory.gd"
 
-func _spawn_element(element: Resource) -> Resource:
-	var instance = ._spawn_element(element)
+func _spawn_element(element: Resource, _display_banned: float = 0, animated_entrance = false) -> InventoryElement:
+	var instance = ._spawn_element(element, _display_banned, animated_entrance)
 
 	if element is CharacterData:
 		var is_unfinished = true
@@ -9,7 +9,7 @@ func _spawn_element(element: Resource) -> Resource:
 			var progress = ProgressData.get_character_difficulty_info(element.my_id, zone.my_id)
 			if progress.max_difficulty_beaten.difficulty_value != -1:
 				is_unfinished = false
-		if is_unfinished == true:
+		if not is_unfinished:
 			instance.add_unfinished_marker()
-
+	
 	return instance
